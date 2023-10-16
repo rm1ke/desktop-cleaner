5891f1300d2097127c6f2e77a4dd92cb4497674a
rm1ke <mikeriecker@gmail.com>
2023-10-15 23:33:24 -0700

    files now go into date folders for archive effect

--- DesktopCleaner.py
+++ DesktopCleaner.py
@@ -1,15 +1,20 @@
 import os
 import shutil
+from datetime import datetime
 
 def organize_desktop():
-    # Path to your desktop
+    # Path to desktop
     desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
 
-    # List of file extensions to organize
+    # List of file extensions to organize..I would later like to add a way to have a
+    # config file that the exe looks for and you can config your own file extension types
     extensions_to_organize = [
         '.pdf', '.txt', '.rpt', '.rdl',
         '.xls', '.xlsx', '.doc', '.docx'
     ]
+
+    # Get today's date to create a subfolder for archiving
+    today_date = datetime.today().strftime('%Y-%m-%d')
 
     for item in os.listdir(desktop_path):
         item_path = os.path.join(desktop_path, item)
@@ -26,14 +31,15 @@ def organize_desktop():
             continue
 
         # Determine the destination directory
-        destination_directory = os.path.join(desktop_path, file_extension.lstrip('.').upper())
-
-        # Create the destination directory if it doesn't exist
-        if not os.path.exists(destination_directory):
-            os.makedirs(destination_directory)
+        extension_folder = os.path.join(desktop_path, file_extension.lstrip('.').upper())
+        date_subfolder = os.path.join(extension_folder, today_date)
+        
+        # Create the destination directories if they don't exist
+        if not os.path.exists(date_subfolder):
+            os.makedirs(date_subfolder)
 
         # Move the file
-        shutil.move(item_path, os.path.join(destination_directory, item))
+        shutil.move(item_path, os.path.join(date_subfolder, item))
 
 # Call the function to organize the desktop
 organize_desktop()
--- README.txt
+++ README.txt
@@ -1,4 +1,6 @@
-This is a simple desktop cleaner, that will organize loose files into folders. 
+This is a simple desktop cleaner, that will organize files into folders, based on the date they are organized, to kind of 
+create an "Archive" effect. For me, I plan on having it as a shortcut, and occasionally click it to organize loose files on my work
+desktop into organized folders, with dates of each "backup" essentially.
 
 It organizes based on file extension type, and if the folder exists 
 for that extension type, it throws it into that folder. Shortcut is nice to periodically use 
